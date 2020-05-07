c1f89582af5d699e8702d46446625142320e24eb
jdfox <jfoxgithub@gmail.com>
2020-05-07 19:57:48 +0100

    add severity level to initial config

--- ras_rm_auth_service/logger_config.py
+++ ras_rm_auth_service/logger_config.py
@@ -55,7 +55,7 @@ def logger_initial_config(log_level=None):
         return event_dict
 
     logging.basicConfig(stream=sys.stdout, level=log_level, format=logger_format)
-    configure(processors=[zipkin_ids, add_log_level, filter_by_level, add_service,
+    configure(processors=[add_severity_level, add_log_level, filter_by_level, add_service,
                           TimeStamper(fmt=logger_date_format, utc=True, key="created_at"), JSONRenderer(indent=indent)])
     oauth_log = logging.getLogger("requests_oauthlib")
     oauth_log.addHandler(logging.NullHandler())
