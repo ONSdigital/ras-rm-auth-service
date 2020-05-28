bd330bcb5b2b8efb02fdac8f8a3023cb5446df0f
Adam Mann <manny160389@hotmail.com>
2020-05-28 12:54:06 +0100

    Change port to standard db port

--- config.py
+++ config.py
@@ -5,13 +5,12 @@ class Config(object):
     DEBUG = False
     TESTING = False
     VERSION = '0.3.0'
-    NAME = 'ras-rm-auth-service'
     DATABASE_SCHEMA = 'auth'
     PORT = os.getenv('PORT', 8041)
     LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', 'INFO')
     SECURITY_USER_NAME = os.getenv('SECURITY_USER_NAME')
     SECURITY_USER_PASSWORD = os.getenv('SECURITY_USER_PASSWORD')
-    DATABASE_URI = os.getenv('DATABASE_URI', "postgresql://postgres:postgres@localhost:6432/postgres")
+    DATABASE_URI = os.getenv('DATABASE_URI', "postgresql://postgres:postgres@localhost:5432/postgres")
 
 
 class DevelopmentConfig(Config):
@@ -26,4 +25,4 @@ class TestingConfig(Config):
     Testing = True
     SECURITY_USER_NAME = 'admin'
     SECURITY_USER_PASSWORD = 'secret'
-    DATABASE_URI = os.getenv("TEST_DATABASE_URI", "postgresql://postgres:postgres@localhost:6432/postgres")
+    DATABASE_URI = os.getenv("TEST_DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres")
