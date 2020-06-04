206b83e009420b583159955e39eab9d6b3ec89bd
Adam Wilkie <Adam.Wilkie@ons.gov.uk>
2020-06-04 15:29:56 +0100

    added last login date column

--- ras_rm_auth_service/models/models.py
+++ ras_rm_auth_service/models/models.py
@@ -1,4 +1,5 @@
 import logging
+import datetime
 from distutils.util import strtobool
 
 from marshmallow import Schema, fields, validate
@@ -26,6 +27,7 @@ class User(Base):
     account_verified = Column(Boolean, default=False, nullable=False)
     account_locked = Column(Boolean, default=False, nullable=False)
     failed_logins = Column(Integer, default=0, nullable=False)
+    last_login_date = Column(datetime, default=None)
 
     def update_user(self, update_params):
         self.username = update_params.get('new_username', self.username)
@@ -78,8 +80,12 @@ class User(Base):
             raise Unauthorized(description=ACCOUNT_NOT_VERIFIED)
 
         self.reset_failed_logins()
+        self.update_last_login_date()
 
         return True
+
+    def update_last_login_date(self):
+        self.last_login_date = datetime.date.today()
 
 
 class AccountSchema(Schema):
