32fc9683d8c29e9b05115964753ffb222d5b22c8
Adam Wilkie <Adam.Wilkie@ons.gov.uk>
2020-06-10 10:56:19 +0100

    added last login date column to migration script

--- migrations/manual_scripts/migrate_users.sql
+++ migrations/manual_scripts/migrate_users.sql
@@ -1,3 +1,3 @@
-INSERT INTO auth.user(id, username, hashed_password, account_verified, account_locked, failed_logins)
-SELECT id, email, password, account_is_verified, account_is_locked, failed_logins
+INSERT INTO auth.user(id, username, hashed_password, account_verified, account_locked, failed_logins, last_login_date)
+SELECT id, email, password, account_is_verified, account_is_locked, failed_logins, last_login_date
 FROM public.credentials_oauthuser;
