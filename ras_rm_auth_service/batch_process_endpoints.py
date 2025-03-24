f3368d3981d7cd438a1bca5911fc2b2e37cfdcb5
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2025-03-24 15:53:02 +0000

    add new log lines

--- ras_rm_auth_service/batch_process_endpoints.py
+++ ras_rm_auth_service/batch_process_endpoints.py
@@ -39,10 +39,13 @@ def delete_accounts():
         logger.info("Scheduler deleting users marked for deletion")
         with transactional_session() as session:
             marked_for_deletion_users = session.query(User).filter(User.mark_for_deletion == True)  # noqa
-            if marked_for_deletion_users.count() > 0:
+            marked_for_deletion_count = marked_for_deletion_users.count()
+            logger.info(f"{marked_for_deletion_count} users marked for deletion")
+            if marked_for_deletion_count > 0:
                 logger.info("sending request to party service to remove ")
                 delete_party_respondents_and_auth_user(marked_for_deletion_users, session)
-                logger.info("Scheduler successfully deleted users marked for deletion")
+                successfully_deleted_count = marked_for_deletion_count - marked_for_deletion_users.count()
+                logger.info(f"Scheduler successfully deleted {successfully_deleted_count} users marked for deletion")
             else:
                 logger.info("No user marked for deletion at this time. Nothing to delete.")
 
